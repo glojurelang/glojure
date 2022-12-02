af0743f5efc9cabcb2b45ef67ea9c341eddd70d6
James Hamlin <james@goforward.com>
2022-12-01 17:55:37 -0800

    Support for executing a program from a file
    Signed-off-by: James Hamlin <james@goforward.com>

--- cmd/glj/main.go
+++ cmd/glj/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"log"
+	"os"
 
+	"github.com/glojurelang/glojure/reader"
 	"github.com/glojurelang/glojure/repl"
+	"github.com/glojurelang/glojure/runtime"
 )
 
 func main() {
@@ -11,5 +16,22 @@ func main() {
 
 	if flag.NArg() == 0 {
 		repl.Start()
+	} else if flag.NArg() == 1 {
+		file, err := os.Open(flag.Arg(0))
+		if err != nil {
+			log.Fatal(err)
+		}
+		rdr := reader.New(bufio.NewReader(file))
+		vals, err := rdr.ReadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+		env := runtime.NewEnvironment(runtime.WithStdout(os.Stdout))
+		for _, val := range vals {
+			_, err := env.Eval(val)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
