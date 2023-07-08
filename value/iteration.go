f76fbb5900d3dd9069b2b4d4dd844d7df8fe5dc4
James Hamlin <james@goforward.com>
2023-07-08 08:59:23 -0700

    Implement reduce interfaces for iterator types
    Signed-off-by: James Hamlin <james@goforward.com>

--- value/iteration.go
+++ value/iteration.go
@@ -101,30 +101,59 @@ type rangeIterator struct {
 	start, end, step int64
 }
 
-func (i rangeIterator) xxx_sequential() {}
+var (
+	_ ISeq        = (*rangeIterator)(nil)
+	_ Sequential  = (*rangeIterator)(nil)
+	_ IReduce     = (*rangeIterator)(nil)
+	_ IReduceInit = (*rangeIterator)(nil)
+)
 
-func (i rangeIterator) Seq() ISeq {
-	return i
+func (r rangeIterator) xxx_sequential() {}
+
+func (r rangeIterator) Seq() ISeq {
+	return r
 }
 
-func (i rangeIterator) First() interface{} {
-	return i.start
+func (r rangeIterator) First() interface{} {
+	return r.start
 }
 
-func (i rangeIterator) Next() ISeq {
-	next := i.start + i.step
-	if next >= i.end {
+func (r rangeIterator) Next() ISeq {
+	next := r.start + r.step
+	if next >= r.end {
 		return nil
 	}
-	return &rangeIterator{start: next, end: i.end, step: i.step}
+	return &rangeIterator{start: next, end: r.end, step: r.step}
 }
 
-func (i rangeIterator) More() ISeq {
-	nxt := i.Next()
+func (r rangeIterator) More() ISeq {
+	nxt := r.Next()
 	if nxt == nil {
 		return emptyList
 	}
 	return nxt
+}
+
+func (r rangeIterator) Reduce(f IFn) interface{} {
+	var ret interface{} = r.start
+	for i := r.start + r.step; i < r.end; i += r.step {
+		ret = f.Invoke(ret, i)
+		if IsReduced(ret) {
+			return ret.(IDeref).Deref()
+		}
+	}
+	return ret
+}
+
+func (r rangeIterator) ReduceInit(f IFn, start interface{}) interface{} {
+	var ret interface{} = start
+	for i := r.start; i < r.end; i += r.step {
+		ret = f.Invoke(ret, i)
+		if IsReduced(ret) {
+			return ret.(IDeref).Deref()
+		}
+	}
+	return ret
 }
 
 // NewValueIterator returns a lazy sequence of the values of x.
@@ -305,100 +334,4 @@ func (i sliceIterator) ReduceInit(f IFn, start interface{}) interface{} {
 		return ret.(IDeref).Deref()
 	}
 	return ret
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-type Repeat struct {
-	x     interface{}
-	count int64
-	next  ISeq
-}
-
-var (
-	_ ISeq        = (*Repeat)(nil)
-	_ Sequential  = (*Repeat)(nil)
-	_ IReduce     = (*Repeat)(nil)
-	_ IReduceInit = (*Repeat)(nil)
-)
-
-func NewRepeat(x interface{}) *Repeat {
-	return &Repeat{x: x, count: -1}
-}
-
-func NewRepeatN(count int64, x interface{}) ISeq {
-	if count <= 0 {
-		return emptyList
-	}
-	return &Repeat{x: x, count: count}
-}
-
-func (r *Repeat) xxx_sequential() {}
-
-func (r *Repeat) First() interface{} {
-	return r.x
-}
-
-func (r *Repeat) More() ISeq {
-	s := r.Next()
-	if s == nil {
-		return emptyList
-	}
-	return s
-}
-
-func (r *Repeat) Next() ISeq {
-	if r.next != nil {
-		return r.next
-	}
-	if r.count > 1 {
-		r.next = NewRepeatN(r.count-1, r.x)
-	} else if r.count == -1 {
-		r.next = r
-	}
-	return r.next
-}
-
-func (r *Repeat) Seq() ISeq {
-	return r
-}
-
-func (r *Repeat) Reduce(f IFn) interface{} {
-	ret := r.x
-	if r.count == -1 {
-		for {
-			ret = f.Invoke(ret, r.x)
-			if IsReduced(ret) {
-				return ret.(IDeref).Deref()
-			}
-		}
-	} else {
-		for i := int64(1); i < r.count; i++ {
-			ret = f.Invoke(ret, r.x)
-			if IsReduced(ret) {
-				return ret.(IDeref).Deref()
-			}
-		}
-		return ret
-	}
-}
-
-func (r *Repeat) ReduceInit(f IFn, start interface{}) interface{} {
-	ret := start
-	if r.count == -1 {
-		for {
-			ret = f.Invoke(ret, r.x)
-			if IsReduced(ret) {
-				return ret.(IDeref).Deref()
-			}
-		}
-	} else {
-		for i := int64(0); i < r.count; i++ {
-			ret = f.Invoke(ret, r.x)
-			if IsReduced(ret) {
-				return ret.(IDeref).Deref()
-			}
-		}
-		return ret
-	}
 }
