5a4a30b4cd1143a2c084f0d1e8c3962ddec64727
James Hamlin <jfhamlin@gmail.com>
2023-08-13 14:06:11 -0700

    Generate OS-specific package maps
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- scripts/gen-gljimports.sh
+++ scripts/gen-gljimports.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
-IMPORTS=$(go run ./cmd/gen-import-interop/main.go)
-echo "$IMPORTS" > $1
+set -e
+
+# make a temp directory with mktemp and build the project there
+DIR=$(mktemp -d)
+EXE="${DIR}/gen-import-interop"
+
+go build -o "${EXE}" ./cmd/gen-import-interop
+
+OUTPUT_FILE=$1
+PLATFORM=$2
+
+IFS='_' read -ra OS_ARCH <<< "$PLATFORM"
+
+OS=${OS_ARCH[0]}
+ARCH=${OS_ARCH[1]}
+
+if [ "$ARCH" == "" ]; then
+    BUILD_TAG="$OS"
+else
+    BUILD_TAG="$ARCH && $OS"
+fi
+
+# disable CGO to avoid cross-compilation issues on darwin.
+IMPORTS=$(CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH "$EXE")
+echo "//go:build $BUILD_TAG" > "$OUTPUT_FILE"
+echo >> "$OUTPUT_FILE"
+echo "$IMPORTS" >> "$OUTPUT_FILE"
+
+# clean up
+rm -rf "${DIR}"
