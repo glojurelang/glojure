d9b5dba2094fa364b56c1b501c1011d7fc4f49f4
James Hamlin <james@goforward.com>
2023-08-19 08:57:20 +0100

    Move glj main logic to reusable package
    Signed-off-by: James Hamlin <james@goforward.com>

---
+++ pkg/gljmain/gljmain.go
@@ -0,0 +1,46 @@
+package gljmain
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/glojurelang/glojure/pkg/lang"
+	"github.com/glojurelang/glojure/pkg/reader"
+	"github.com/glojurelang/glojure/pkg/repl"
+	"github.com/glojurelang/glojure/pkg/runtime"
+)
+
+func Main(args []string) {
+	runtime.AddLoadPath(os.DirFS("."))
+
+	if len(args) == 0 {
+		repl.Start()
+	} else {
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		env := lang.GlobalEnv
+
+		core := lang.FindNamespace(lang.NewSymbol("glojure.core"))
+		core.FindInternedVar(lang.NewSymbol("*command-line-args*")).BindRoot(lang.Seq(os.Args[2:]))
+
+		rdr := reader.New(bufio.NewReader(file), reader.WithGetCurrentNS(func() *lang.Namespace {
+			return env.CurrentNamespace()
+		}))
+		for {
+			val, err := rdr.ReadOne()
+			if err == reader.ErrEOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = env.Eval(val)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
