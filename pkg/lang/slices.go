58eb695fe4ec01764f07567f3aaf7e5a340e0f48
James Hamlin <jfhamlin@gmail.com>
2023-09-03 14:10:15 -0700

    Fix glojure.lang.LazilyPersistentVector warning on start
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- pkg/lang/slices.go
+++ pkg/lang/slices.go
@@ -1,8 +1,33 @@
 package lang
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
-func SliceSet(slc interface{}, idx int, val interface{}) {
+func SliceSet(slc any, idx int, val any) {
 	slcVal := reflect.ValueOf(slc)
 	slcVal.Index(idx).Set(reflect.ValueOf(val))
+}
+
+func ToSlice(x any) []any {
+	if IsNil(x) {
+		return nil
+	}
+	if s, ok := x.(ISeq); ok {
+		res := make([]interface{}, 0, Count(x))
+		for s := Seq(s); s != nil; s = s.Next() {
+			res = append(res, s.First())
+		}
+		return res
+	}
+	xVal := reflect.ValueOf(x)
+	if xVal.Kind() == reflect.Slice || xVal.Kind() == reflect.Array {
+		res := make([]interface{}, xVal.Len())
+		for i := 0; i < xVal.Len(); i++ {
+			res[i] = xVal.Index(i).Interface()
+		}
+		return res
+	}
+	panic(fmt.Errorf("ToSlice not supported on type: %T", x))
 }
