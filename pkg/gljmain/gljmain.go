a2bebb9f6db6d0aedbb1c8e24c05d36075db8421
Ingy döt Net <ingy@ingy.net>
2025-08-11 16:47:05 -0700

    Add command line options for glj -e --version --help -h  (#88)
    * Support 'glj -e ...' commands
    
    * Add *glojure-version* dynamic variable
    
    * Support 'glj --version'
    
    * Support --help and -h CLI options
    
    * Fix 'make test', add 'make build', 'make clean'
    
    The CLI tests require 'bin/<os-arch>/glc' binary to be built in order to
    run 'glj ...' commands and test the output.

--- pkg/gljmain/gljmain.go
+++ pkg/gljmain/gljmain.go
@@ -2,8 +2,10 @@ package gljmain
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	// bootstrap the runtime
 	_ "github.com/glojurelang/glojure/pkg/glj"
@@ -14,20 +16,82 @@ import (
 	"github.com/glojurelang/glojure/pkg/runtime"
 )
 
+func printHelp() {
+	fmt.Printf(`Glojure v%s
+
+Usage: glj [options] [file]
+
+Options:
+  -e <expr>        Evaluate expression from command line
+  -h, --help       Show this help message
+  --version        Show version information
+
+Examples:
+  glj                   # Start REPL
+  glj -e "(+ 1 2)"      # Evaluate expression
+  glj script.glj        # Run script file
+  glj --version         # Show version
+  glj --help            # Show this help
+
+For more information, visit: https://github.com/glojurelang/glojure
+`, runtime.VERSION)
+}
+
 func Main(args []string) {
 	runtime.AddLoadPath(os.DirFS("."))
 
 	if len(args) == 0 {
 		repl.Start()
+	} else if args[0] == "--version" {
+		fmt.Printf("glojure v%s\n", runtime.VERSION)
+		return
+	} else if args[0] == "--help" || args[0] == "-h" {
+		printHelp()
+		return
+	} else if args[0] == "-e" {
+		// Evaluate expression from command line
+		if len(args) < 2 {
+			log.Fatal("glj: -e requires an expression")
+		}
+		expr := args[1]
+		env := lang.GlobalEnv
+
+		// Set command line args (everything after -e and the expression)
+		core := lang.FindNamespace(lang.NewSymbol("glojure.core"))
+		core.FindInternedVar(lang.NewSymbol("*command-line-args*")).BindRoot(lang.Seq(args[2:]))
+
+		rdr := reader.New(strings.NewReader(expr), reader.WithGetCurrentNS(func() *lang.Namespace {
+			return env.CurrentNamespace()
+		}))
+		var lastResult interface{}
+		for {
+			val, err := rdr.ReadOne()
+			if err == reader.ErrEOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			result, err := env.Eval(val)
+			if err != nil {
+				log.Fatal(err)
+			}
+			lastResult = result
+		}
+		// Print only the final result unless it's nil
+		if !lang.IsNil(lastResult) {
+			fmt.Println(lang.PrintString(lastResult))
+		}
 	} else {
-		file, err := os.Open(os.Args[1])
+		// Execute file
+		file, err := os.Open(args[0])
 		if err != nil {
 			log.Fatal(err)
 		}
 		env := lang.GlobalEnv
 
 		core := lang.FindNamespace(lang.NewSymbol("glojure.core"))
-		core.FindInternedVar(lang.NewSymbol("*command-line-args*")).BindRoot(lang.Seq(os.Args[2:]))
+		core.FindInternedVar(lang.NewSymbol("*command-line-args*")).BindRoot(lang.Seq(args[1:]))
 
 		rdr := reader.New(bufio.NewReader(file), reader.WithGetCurrentNS(func() *lang.Namespace {
 			return env.CurrentNamespace()
