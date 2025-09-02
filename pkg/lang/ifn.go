8e7918b3824a024cda089b12668c9953001a2b42
James Hamlin <jfhamlin@gmail.com>
2025-09-01 21:44:54 -0700

    Revert FnFunc struct wrapping
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- pkg/lang/ifn.go
+++ pkg/lang/ifn.go
@@ -1,39 +1,29 @@
 package lang
 
 // FnFunc is a wrapped Go function that implements the IFn interface.
-type FnFunc struct {
-	fn   func(args ...any) any
-	meta IPersistentMap
-}
+type FnFunc func(args ...any) any
 
 var (
-	_ IFn = FnFunc{}
+	_ IFn = FnFunc(nil)
 )
 
 func NewFnFunc(fn func(args ...any) any) FnFunc {
-	return FnFunc{
-		fn: fn,
-	}
+	return FnFunc(fn)
 }
 
 func (f FnFunc) Invoke(args ...any) any {
-	return f.fn(args...)
+	return f(args...)
 }
 
 func (f FnFunc) ApplyTo(args ISeq) any {
-	return f.fn(seqToSlice(args)...)
+	return f(seqToSlice(args)...)
 }
 
 func (f FnFunc) Meta() IPersistentMap {
-	return f.meta
+	return nil
 }
 
 func (f FnFunc) WithMeta(meta IPersistentMap) any {
-	if f.meta == meta {
-		return f
-	}
-
-	cpy := f
-	cpy.meta = meta
-	return cpy
+	// no-op
+	return f
 }
