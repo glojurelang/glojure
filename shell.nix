35ad477d1cbb9023476b40a07085a2a0b599559f
James Hamlin <jfhamlin@gmail.com>
2025-10-06 12:58:20 -0700

    Bump go to 1.24
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- shell.nix
+++ shell.nix
@@ -6,11 +6,17 @@
 
 pkgs.mkShell {
   nativeBuildInputs = [
-    pkgs.go_1_21
+    pkgs.go_1_24
     pkgs.clojure
   ];
 
   shellHook = ''
         export MACOSX_DEPLOYMENT_TARGET=12.0
+
+        # Remove xcbuild's xcrun from PATH
+        export PATH=$(echo "$PATH" | sed "s|${pkgs.xcbuild.xcrun}/bin||g")
+
+        # Let Xcode pick its own developer dir
+        unset DEVELOPER_DIR
     '';
 }
